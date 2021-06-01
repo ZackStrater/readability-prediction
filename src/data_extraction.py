7c23079a5bedc5f4960e7ea9e3cab2ffd0a140c7
ZackStrater <zack.m.strater@gmail.com>
2021-06-01 10:50:35 -0700

    adding and formatting extra text

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -7,22 +7,19 @@ import nltk
 from nltk.tokenize import sent_tokenize
 import re
 
-df = pd.read_csv('../data/train_extra.csv', encoding = "ISO-8859-1")
+df = pd.read_csv('../data/train_extra_200.csv', encoding = "ISO-8859-1")
 df['length_excerpt'] = df['excerpt'].str.len()
 df['num_sentences'] = df['excerpt'].apply(lambda x: len(sent_tokenize(x)))
-df['excerpt'] = df['excerpt'].apply(lambda x: re.sub(r'\n', '', x))
-df['full_text'] = df['full_text'].apply(lambda x: re.sub(r'\n', '', str(x)))
-df['len_full_text'] = df['full_text'].str.len()
-print(df.head(100))
-print(df.max())
-print(df.min())
-# for i in range(100):
-#     print(i)
-#     print(df['excerpt'][i])
-#     print('\n')
-#     print(df['full_text'][i])
-#     print('\n')
+df['excerpt'] = df['excerpt'].apply(lambda x: re.sub(r'\n+', ' ', x))
+df['full_text'] = df['full_text'].apply(lambda x: re.sub(r'\n+', ' ', str(x)))
 
+df['excerpt'] = df['excerpt'].apply(lambda x: re.sub(r'\"\"', '\"', x))
+df['full_text'] = df['full_text'].apply(lambda x: re.sub(r'\"\"', '\"', str(x)))
+
+df['len_full_text'] = df['full_text'].str.len()
+# print(df.head(100))
+# print(df.max())
+# print(df.min())
 
 def chunk_indices(sentence_lengths, min_chars=800):
     count = 0
@@ -39,7 +36,8 @@ def chunk_indices(sentence_lengths, min_chars=800):
 
 def split_text_into_sections(text, target, std_error, min_chars=800):
     if text != 'nan':
-        cleaned_text = re.sub(r'\[.+?]', '', text)
+        rev1_text = re.sub(r'\[.+?]', '', text)
+        cleaned_text = rev1_text
         sentences = sent_tokenize(cleaned_text)
         sentence_lengths = [len(sent) for sent in sentences]
         chunks_idx = chunk_indices(sentence_lengths, min_chars=min_chars)
@@ -49,7 +47,7 @@ def split_text_into_sections(text, target, std_error, min_chars=800):
             stop = idx[1]
             paragraph = ''
             for c in range(start, stop):
-                paragraph += sentences[c]
+                paragraph += f'{sentences[c]} '
             chunk_bin.append(paragraph)
         targets = np.full(len(chunk_bin), target)
         std_errors = np.full(len(chunk_bin), std_error)
@@ -59,4 +57,9 @@ def split_text_into_sections(text, target, std_error, min_chars=800):
 
 extra_texts = [split_text_into_sections(row['full_text'], row['target'], row['standard_error']) for i, row in df.iterrows()]
 extra_text_df = pd.concat(extra_texts)
-print(extra_text_df[0:400])
+print(extra_text_df)
+
+print(df)
+
+df_combined = pd.concat([df, extra_text_df])
+df_combined['target'] = df['target']
